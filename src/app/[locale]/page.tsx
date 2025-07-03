c0f45fcebe19db07d62f39979a48af353c954886
Esteban V.M. <estebanvm90@outlook.com>
2025-07-03 12:31:23 -0300

    add home layout and page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,19 @@
+import type { Namespace } from 'i18next'
+import initTranslations from '@/app/init-translations'
+import { TranslationProvider } from '@/components'
+import { verifySession } from '@/lib/auth-utils'
+
+export default async function HomePage({ params }: Props.WithParams) {
+  await verifySession()
+
+  const { locale } = await params
+  const namespace: Namespace = 'home'
+  const { t, resources } = await initTranslations(locale, namespace)
+
+  return (
+    <TranslationProvider locale={locale} namespace={namespace} resource={resources}>
+      <div>Home Page</div>
+      <span>{t('title')} </span>
+    </TranslationProvider>
+  )
+}

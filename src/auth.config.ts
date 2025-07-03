4b387da38f91eec56d8102b196d079fa0787607f
Esteban V.M. <estebanvm90@outlook.com>
2025-07-03 11:58:48 -0300

    add auth and i18n
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/auth.config.ts
@@ -0,0 +1,43 @@
+import type { NextAuthConfig } from 'next-auth'
+import { compare } from 'bcryptjs'
+import Credentials from 'next-auth/providers/credentials'
+import { APP_ROUTES } from '@/lib/constants'
+import { CustomAuthError } from '@/lib/errors'
+import { prisma } from '@/prisma'
+
+export default {
+  trustHost: true,
+  pages: {
+    signIn: APP_ROUTES.LOGIN,
+  },
+  callbacks: {
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub
+      }
+
+      return session
+    },
+  },
+  providers: [
+    Credentials({
+      async authorize(credentials): Promise<Models.User> {
+        const { email, password } = credentials as Schemas.Login
+        const userFromDB = await prisma.user.findUnique({ where: { email } })
+
+        if (!userFromDB) {
+          throw new CustomAuthError(`The email ${email} is not registered yet.`)
+        }
+
+        const isLoggedIn = await compare(password, userFromDB.password)
+
+        if (!isLoggedIn) {
+          throw new CustomAuthError('Invalid password, try again')
+        }
+
+        const { password: _, ...loggedInUser } = userFromDB
+        return loggedInUser satisfies Models.User
+      },
+    }),
+  ],
+} satisfies NextAuthConfig
---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import NextAuth from 'next-auth'
+import { i18nRouter } from 'next-i18n-router'
+import authConfig from '@/auth.config'
+import i18nConfig from '@/i18n.config'
+
+const { auth } = NextAuth(authConfig)
+
+export default auth((request) => {
+  return i18nRouter(request, i18nConfig)
+})
+
+export const config = {
+  matcher: '/((?!api|static|.*\\..*|_next).*)',
+}

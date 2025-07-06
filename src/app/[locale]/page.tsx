cb3ea9ed7b11f4a4986c9d4bf4e97914ad789065
Esteban V.M. <estebanvm90@outlook.com>
2025-07-06 15:14:31 -0300

    update home page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,19 +1,11 @@
-import type { Namespace } from 'i18next'
-import initTranslations from '@/app/init-translations'
-import { TranslationProvider } from '@/components'
 import { verifySession } from '@/lib/auth-utils'
 
-export default async function HomePage({ params }: Props.PageProps) {
+export default async function HomePage() {
   await verifySession()
 
-  const namespace: Namespace = 'home'
-  const { locale } = await params
-  const { t, resource } = await initTranslations(locale, namespace)
-
   return (
-    <TranslationProvider locale={locale} namespace={namespace} resource={resource}>
+    <>
       <div>Home Page</div>
-      <span>{t('title')} </span>
-    </TranslationProvider>
+    </>
   )
 }

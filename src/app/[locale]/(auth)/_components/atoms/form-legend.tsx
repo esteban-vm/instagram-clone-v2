01ee630a5fbb77a849a0816054067e474336b3dd
Esteban V.M. <estebanvm90@outlook.com>
2025-07-06 15:14:45 -0300

    update form legend
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/[locale]/(auth)/_components/atoms/form-legend.tsx
+++ src/app/[locale]/(auth)/_components/atoms/form-legend.tsx
@@ -2,7 +2,7 @@ import { Fieldset } from 'rsc-daisyui'
 
 export function FormLegend() {
   return (
-    <Fieldset.Legend className='instagram-gradient-overlay mx-auto font-playwrite text-3xl'>
+    <Fieldset.Legend className='Instagram__overlay mx-auto font-playwrite text-3xl'>
       <span className='pb-1'>Instagram</span>
     </Fieldset.Legend>
   )

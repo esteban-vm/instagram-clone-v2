2a5485ace6adc5e7ad7ef464ec238f765cd066ea
Esteban V.M. <estebanvm90@outlook.com>
2025-07-07 11:27:41 -0300

    add translations for navbar
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/navigation/widgets/language-changer.tsx
+++ src/components/navigation/widgets/language-changer.tsx
@@ -1,18 +1,17 @@
+import { useTranslation } from 'react-i18next'
 import { Toggle } from 'rsc-daisyui'
 import { SpainFlag, UKFlag } from '@/components/navigation/icons'
 import { useLanguageChanger } from '@/hooks'
 
 export function LanguageChanger() {
+  const { t } = useTranslation('navigation', { keyPrefix: 'language_changer' })
   const { currentLanguage, defaultLanguage, changeLanguage } = useLanguageChanger()
+  const isChecked = currentLanguage === defaultLanguage
 
   return (
-    <label className='Navigation__changer ml-2'>
+    <label className='Navigation__changer ml-2' title={t('title')}>
       <SpainFlag />
-      <Toggle
-        className='Navigation__toggle'
-        defaultChecked={currentLanguage === defaultLanguage}
-        onChange={changeLanguage}
-      />
+      <Toggle className='Navigation__toggle' defaultChecked={isChecked} onChange={changeLanguage} />
       <UKFlag />
     </label>
   )
--- src/components/navigation/widgets/theme-changer.tsx
+++ src/components/navigation/widgets/theme-changer.tsx
@@ -1,19 +1,22 @@
 import { useTheme } from 'next-themes'
+import { useTranslation } from 'react-i18next'
 import { FaRegMoon, FaRegSun } from 'react-icons/fa6'
 import { Toggle } from 'rsc-daisyui'
 
 export function ThemeChanger() {
+  const { t } = useTranslation('navigation', { keyPrefix: 'theme_changer' })
   const { theme, themes, setTheme } = useTheme()
+  const isLight = theme === 'light'
 
   const changeTheme = () => {
-    const newTheme = themes.find((t) => t !== theme)
+    const newTheme = themes.find((currentTheme) => currentTheme !== theme)
     if (newTheme) setTheme(newTheme)
   }
 
   return (
-    <label className='Navigation__changer'>
+    <label className='Navigation__changer' title={isLight ? t('title_dark') : t('title_light')}>
       <FaRegMoon className='size-6 fill-violet-500' />
-      <Toggle className='Navigation__toggle' defaultChecked={theme === 'light'} onChange={changeTheme} />
+      <Toggle className='Navigation__toggle' defaultChecked={isLight} onChange={changeTheme} />
       <FaRegSun className='size-6 fill-amber-500' />
     </label>
   )

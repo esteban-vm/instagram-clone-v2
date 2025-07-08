23a6d43622923f2ae32a0b1654a838c08a28fda2
Esteban V.M. <estebanvm90@outlook.com>
2025-07-08 15:56:05 -0300

    create not found
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/app/[locale]/not-found.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Page not found',
+}
+
+export default function NotFound() {
+  return <div>Page not found</div>
+}

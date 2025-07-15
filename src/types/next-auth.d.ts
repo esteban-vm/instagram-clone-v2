587c8545c4b4dc429614d4335d8b0480aee312f2
Esteban V.M. <estebanvm90@outlook.com>
2025-07-15 09:19:48 -0300

    update next auth types
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,7 +1,21 @@
+import type { Role } from '@prisma/client'
 import 'next-auth'
+import 'next-auth/jwt'
 
 declare module 'next-auth' {
+  interface User extends Models.User {
+    id: string
+    name: string
+    email: string
+  }
+
   interface Session {
     user: Models.User
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    role?: Role | null
   }
 }

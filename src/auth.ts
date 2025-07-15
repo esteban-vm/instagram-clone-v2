c9dcc63ed5e44907703fcfe3a9bb28df4f37d08a
Esteban V.M. <estebanvm90@outlook.com>
2025-07-15 09:22:08 -0300

    update auth.ts
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/auth.ts
+++ src/auth.ts
@@ -4,6 +4,8 @@ import authConfig from '@/auth.config'
 import { prisma } from '@/prisma'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   adapter: PrismaAdapter(prisma),
   session: { strategy: 'jwt' },
   ...authConfig,

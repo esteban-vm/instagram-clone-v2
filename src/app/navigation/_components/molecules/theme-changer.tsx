a8ab4c26e4120f47a38d5eb38536f9cdb51f84f8
Esteban V.M. <estebanvm90@outlook.com>
2025-07-18 15:21:40 -0300

    fix theme changer
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/navigation/_components/molecules/theme-changer.tsx
+++ src/app/navigation/_components/molecules/theme-changer.tsx
@@ -1,4 +1,5 @@
 import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Menu, Toggle } from 'rsc-daisyui'
 import { Atoms } from '@/app/navigation/_components'
@@ -6,18 +7,22 @@ import { Atoms } from '@/app/navigation/_components'
 export function ThemeChanger() {
   const { t } = useTranslation('navigation', { keyPrefix: 'theme_changer' })
   const { theme, themes, setTheme } = useTheme()
-  const isLight = theme === 'light'
+  const [isChecked, setIsChecked] = useState(false)
+
+  useEffect(() => {
+    setIsChecked(theme === 'light')
+  }, [theme])
 
   const changeTheme = () => {
-    const newTheme = themes.find((currentTheme) => currentTheme !== theme)
+    const newTheme = themes.find((t) => t !== theme)
     if (newTheme) setTheme(newTheme)
   }
 
   return (
     <Menu.Item>
-      <label className='Navigation__changer' title={isLight ? t('dark') : t('light')}>
+      <label className='Navigation__changer' title={isChecked ? t('dark') : t('light')}>
         <Atoms.MoonIcon className='size-8 rounded-sm bg-indigo-200 p-0.5' />
-        <Toggle className='Navigation__toggle' defaultChecked={isLight} onChange={changeTheme} />
+        <Toggle checked={isChecked} className='Navigation__toggle' onChange={changeTheme} />
         <Atoms.SunIcon className='size-8 rounded-sm bg-sky-200 p-0.5' />
       </label>
     </Menu.Item>

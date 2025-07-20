f070b1a991e3aa73cdf53b3184b8784ea594c6b8
Esteban V.M. <estebanvm90@outlook.com>
2025-07-20 16:19:04 -0300

    working on dashboard
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/[locale]/dashboard/@sidebar/page.tsx
+++ src/app/[locale]/dashboard/@sidebar/page.tsx
@@ -1,7 +1,32 @@
+'use client'
+
+import Image from 'next/image'
+import { Avatar, Mask } from 'rsc-daisyui'
+import { useCurrentSession } from '@/hooks'
+import avatar1 from '@/images/avatars/female1.webp'
+import { Texts } from '@/lib/texts'
+
 export default function SidebarPage() {
+  const { currentSession } = useCurrentSession()
+
+  const user = currentSession?.user
+  if (!user) return null
+
+  const { avatar, name } = user
+
   return (
-    <section className='border border-primary'>
-      <span>Sidebar Page</span>
+    <section className='border border-primary p-1.5'>
+      <Avatar placeholder={!avatar}>
+        {avatar ? (
+          <Mask as='div' className='w-24' shape='circle'>
+            <Image alt='avatar' src={avatar1} />
+          </Mask>
+        ) : (
+          <div className='w-24 rounded-full bg-neutral text-neutral-content'>
+            <span className='text-3xl'>{Texts.truncate(name)}</span>
+          </div>
+        )}
+      </Avatar>
     </section>
   )
 }
--- src/app/[locale]/dashboard/layout.tsx
+++ src/app/[locale]/dashboard/layout.tsx
@@ -19,7 +19,7 @@ export default async function DashboardLayout({ children: _, sidebar, timeline }
   if (!session?.user) redirect(APP_ROUTES.LOGIN)
 
   return (
-    <div className='mx-auto mt-16 grid max-w-screen-2xl grid-cols-3 gap-4'>
+    <div className='mx-auto mt-16 grid max-w-screen-2xl gap-4 text-center lg:grid-cols-3'>
       {timeline}
       {sidebar}
     </div>

0642b4326e2b6b9e0081054984baaf90a8761940
Esteban V.M. <estebanvm90@outlook.com>
2025-07-22 16:32:02 -0300

    update scene page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/[locale]/scene/page.tsx
+++ src/app/[locale]/scene/page.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from 'next'
 import { Organisms } from '@/app/[locale]/scene/_components'
+import { verifySession } from '@/lib/auth-utils'
 
 export const metadata: Metadata = {
   title: 'Scene',
 }
 
-export default function ScenePage() {
+export default async function ScenePage() {
+  await verifySession()
+
   return (
     <>
       <Organisms.SolarSystem />

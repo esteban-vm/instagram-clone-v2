33edba2cb3d7024cc0d439c2828b50033122b68e
Esteban V.M. <estebanvm90@outlook.com>
2025-07-23 11:01:42 -0300

    create follow user action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/user.actions.ts
+++ src/actions/user.actions.ts
@@ -1,8 +1,25 @@
 'use server'
 
+import { revalidatePath } from 'next/cache'
 import { mockDelay } from '@/lib/auth-utils'
+import { APP_ROUTES } from '@/lib/constants'
 import { authClient } from '@/lib/safe-action'
+import { SchemaWithId } from '@/lib/validations'
 import { prisma } from '@/prisma'
+
+export const followUser = authClient.schema(SchemaWithId).action(async ({ ctx, parsedInput }) => {
+  await mockDelay(1_000)
+
+  const userId = ctx.user.id
+  const userToFollowId = parsedInput.id
+
+  await prisma.user.update({
+    where: { id: userToFollowId },
+    data: { followers: { create: { followingId: userId } } },
+  })
+
+  revalidatePath(APP_ROUTES.DASHBOARD, 'layout')
+})
 
 export const getSuggestedUsers = authClient.action(async ({ ctx }): Promise<Models.User[]> => {
   await mockDelay(2_000)

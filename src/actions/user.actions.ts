9fa13a9ccbded6e88457fe3e252bf8e4bb6486c3
Esteban V.M. <estebanvm90@outlook.com>
2025-07-24 16:08:48 -0300

    update auth actions
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/user.actions.ts
+++ src/actions/user.actions.ts
@@ -6,26 +6,28 @@ import { authClient } from '@/lib/safe-action'
 import { SchemaWithId } from '@/lib/validations'
 import { prisma } from '@/prisma'
 
-export const followUser = authClient.schema(SchemaWithId).action(async ({ ctx, parsedInput }) => {
-  const userId = ctx.user.id
+export const followUser = authClient.schema(SchemaWithId).action(async ({ ctx, parsedInput }): Promise<Models.User> => {
+  const loggedInUserId = ctx.user.id
   const userToFollowId = parsedInput.id
 
-  await prisma.user.update({
+  const followedUser = await prisma.user.update({
     where: { id: userToFollowId },
-    data: { followers: { create: { followingId: userId } } },
+    data: { followers: { create: { followingId: loggedInUserId } } },
+    omit: { active: true, createdAt: true, updatedAt: true, password: true },
   })
 
   revalidatePath(APP_ROUTES.DASHBOARD, 'layout')
+  return followedUser
 })
 
 export const getSuggestedUsers = authClient.action(async ({ ctx }): Promise<Models.User[]> => {
-  const userId = ctx.user.id
+  const loggedInUserId = ctx.user.id
 
   const suggestedUsers = await prisma.user.findMany({
     take: 10,
     orderBy: { name: 'asc' },
     omit: { active: true, createdAt: true, updatedAt: true, password: true },
-    where: { active: true, id: { not: userId }, followers: { none: { followingId: userId } } },
+    where: { active: true, id: { not: loggedInUserId }, followers: { none: { followingId: loggedInUserId } } },
   })
 
   return suggestedUsers

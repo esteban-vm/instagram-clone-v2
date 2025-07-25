a86b86e2eddfbe1b30ff4f3ddb443ae4d8f7efad
Esteban V.M. <estebanvm90@outlook.com>
2025-07-25 11:26:23 -0300

    test photo actions
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,11 @@
+import { PhotoActions } from '@/actions'
 import { verifySession } from '@/lib/auth-utils'
 
 export default async function HomePage() {
   await verifySession()
+
+  const result = await PhotoActions.getPhotos()
+  console.log({ photos: result?.data?.map((photo) => photo.image) })
 
   return (
     <>

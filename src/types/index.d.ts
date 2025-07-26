0b60c7f7cc27e9639c1fb26e7e5590bd6965450f
Esteban V.M. <estebanvm90@outlook.com>
2025-07-26 08:59:23 -0300

    update types
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,4 +1,3 @@
-import type { Photo as PhotoDB, User as UserDB } from '@prisma/client'
+import type { User as UserDB } from '@prisma/client'
 
-export type Photo = Omit<PhotoDB, 'createdAt' | 'updatedAt'>
 export type User = Omit<UserDB, 'password' | 'active' | 'createdAt' | 'updatedAt'>

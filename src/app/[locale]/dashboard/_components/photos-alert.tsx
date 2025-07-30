1a2847ad6cbc5846cdef0bc906c053be656aeb75
Esteban V.M. <estebanvm90@outlook.com>
2025-07-30 13:31:42 -0300

    update photos alert
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/[locale]/dashboard/_components/photos-alert.tsx
+++ src/app/[locale]/dashboard/_components/photos-alert.tsx
@@ -1,14 +1,18 @@
 import { useTranslation } from 'react-i18next'
-import { BsInfoCircleFill } from 'react-icons/bs'
 import { Alert } from 'rsc-daisyui'
+import { Common as $ } from '@/app/[locale]/dashboard/_styled'
 
 export function PhotosAlert() {
-  const { t } = useTranslation('dashboard', { keyPrefix: 'timeline' })
+  const { t } = useTranslation('dashboard', { keyPrefix: 'timeline.photos_alert' })
 
   return (
-    <Alert className='col-span-2 h-14 justify-self-center italic'>
-      <BsInfoCircleFill className='size-full' />
-      <span>{t('photos_alert')}</span>
+    <Alert className='col-span-2 self-start justify-self-center px-3.5 py-2.5' color='warning'>
+      <$.Alert.Icon />
+      <div>
+        <$.Alert.Title>{t('title')}</$.Alert.Title>
+        <$.Alert.Separator />
+        <$.Alert.Text>{t('text')}</$.Alert.Text>
+      </div>
     </Alert>
   )
 }
